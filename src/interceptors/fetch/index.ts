ab71e0345e387086fd5c5efa3807cbdae88e5baa
Paul Serre <paul27000@gmail.com>
2025-04-08 09:05:14 -0300

    fix: include build for personal use

--- src/interceptors/fetch/index.ts
+++ src/interceptors/fetch/index.ts
@@ -45,7 +45,7 @@ export class FetchInterceptor extends Interceptor<HttpRequestEventMap> {
         typeof input === 'string' &&
         typeof location !== 'undefined' &&
         !canParseUrl(input)
-          ? new URL(input, location.origin)
+          ? new URL(input, location.toString())
           : input
 
       const request = new Request(resolvedInput, init)
